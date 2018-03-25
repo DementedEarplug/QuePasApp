73fe926c8dc38f797f5571d640dc6857a6e42cae
Onix <onix.tarrats@upr.edu>
2018-03-25 15:59:18 -0400

    First crack at user. Partially built handler and dao, get all users route is working

--- routes/index.py
+++ routes/index.py
@@ -1,6 +1,18 @@
 from flask import Flask, request
+from handler.user_handler import UserHandler
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "The beginning";
+    return "The beginning"
+
+'''User Routes'''
+@app.route('/QuePasApp/users')
+def users():
+    handler = UserHandler()
+    return handler.getAllUsers()
+
+if __name__ == '__index__' :
+    app.run()
 
